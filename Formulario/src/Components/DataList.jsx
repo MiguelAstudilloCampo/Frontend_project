b37b553c03885d42a02e010f2ce1e4773d341d47
MiguelAstudilloCampo <132482640+MiguelAstudilloCampo@users.noreply.github.com>
2024-02-18 10:11:48 -0500

    Se agrego componente para ver la lista de registro mas sus estilos

---
+++ Formulario/src/Components/DataList.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import "./style/DataList.css";
+
+const DataList = ({ data, onEdit, onDelete }) => {
+  return (
+    <table className="Tabla">
+      <tbody className="informacion">
+        {data.map((item) => (
+          <tr key={item.id}>
+            <div className="datos">
+              <div className="id">
+                <th>ID</th>
+                <td>{item.id}</td>
+              </div>
+              <div className="nombre">
+                <th>Nombre</th>
+                <td>{item.nombre}</td>
+              </div>
+              <div className="descripcion">
+                <th>Descripcion</th>
+                <td>{item.descripcion}</td>
+              </div>
+            </div>
+            <hr />
+            <td className="Botones">
+              <button className="btn btn-primary" onClick={() => onEdit(item)}>
+                Editar
+              </button>
+              <button
+                className="btn btn-danger"
+                onClick={() => onDelete(item.id)}
+              >
+                Eliminar
+              </button>
+            </td>
+            <hr />
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+export default DataList;
