984829e919225ba608da67d41fb856c66aa928a1
MiguelAstudilloCampo <132482640+MiguelAstudilloCampo@users.noreply.github.com>
2024-02-18 10:40:46 -0500

    Se agrego el ap.css y se codifico el app.jsx

--- Formulario/src/App.jsx
+++ Formulario/src/App.jsx
@@ -1,35 +1,84 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from "react";
+import DataList from "./Components/DataList";
+import CreateModal from "./Components/Modal";
+import EditModal from "./Components/Modal Editable";
+import "./Ap.css";
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [data, setData] = useState([]);
+  const [selectedData, setSelectedData] = useState(null);
+  const [isCreateModalOpen, setCreateModalOpen] = useState(false);
+  const [isEditModalOpen, setEditModalOpen] = useState(false);
+
+  const handleCreate = (newData) => {
+    setData([...data, { ...newData, id: Date.now().toString() }]);
+  };
+
+  const handleEdit = (editedData) => {
+    const updatedData = data.map((item) =>
+      item.id === editedData.id ? editedData : item
+    );
+    setData(updatedData);
+  };
+
+  const handleDelete = (id) => {
+    const updatedData = data.filter((item) => item.id !== id);
+    setData(updatedData);
+  };
+
+  const handleEditModalOpen = (selected) => {
+    setSelectedData(selected);
+    setEditModalOpen(true);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div className="Content">
+      <div className="registro_datos">
+        <h1>Registro de Datos</h1>
 
-export default App
+        <button
+          className="btn btn-success"
+          onClick={() => setCreateModalOpen(true)}
+        >
+          Crear Registro
+        </button>
+      </div>
+
+      <div className="Registro">
+        <div className="CrearRegistro">
+          <CreateModal
+            isOpen={isCreateModalOpen}
+            onClose={() => setCreateModalOpen(false)}
+            onCreate={handleCreate}
+          />
+        </div>
+        <div className="RR">
+          <h3>Registro</h3>
+          <div className="contenedor">
+            <div className="contenido">
+              <DataList
+                data={data}
+                onEdit={handleEditModalOpen}
+                onDelete={handleDelete}
+              />
+            </div>
+          </div>
+        </div>
+
+        <div className="EditarRegistro">
+          {selectedData && (
+            <EditModal
+              isOpen={isEditModalOpen}
+              onClose={() => setEditModalOpen(false)}
+              onUpdate={handleEdit}
+              selectedData={selectedData}
+            />
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
